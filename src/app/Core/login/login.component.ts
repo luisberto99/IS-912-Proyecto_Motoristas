8e0a703eb1ccd37a0aee587ddf5d10ab5fdee2ef
Luis Banegas <55450718+Shadow504@users.noreply.github.com>
2021-07-23 20:32:58 -0600

    Creacion componente registro y rutas necesarias

---
+++ src/app/Core/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
