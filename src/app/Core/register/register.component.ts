f193c918f88bb51acda2a247cedc14153626654f
Luis Banegas <55450718+Shadow504@users.noreply.github.com>
2021-07-23 20:10:39 -0600

    Validaciones formulario Registro

--- src/app/Core/register/register.component.ts
+++ src/app/Core/register/register.component.ts
@@ -10,14 +10,14 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   formRegister = new FormGroup({
-    primerNombre: new FormControl("",[Validators.maxLength(20)]),
-    primerApellido: new FormControl("",[Validators.maxLength(20)]),
-    identidad: new FormControl("",[Validators.maxLength(13)]),
-    domicilio: new FormControl("",[Validators.maxLength(40)]),
-    telefono: new FormControl("",[Validators.maxLength(40)]),
-    email: new FormControl("",[Validators.email]),
-    password: new FormControl("",[]),
-    passwordConfirm: new FormControl("",[]),
+    primerNombre: new FormControl("",[Validators.required, Validators.maxLength(20)]),
+    primerApellido: new FormControl("",[Validators.required, Validators.maxLength(20)]),
+    identidad: new FormControl("",[Validators.required, Validators.pattern('^([0-9]){13}$')]),
+    domicilio: new FormControl("",[Validators.required, Validators.maxLength(40)]),
+    telefono: new FormControl("",[Validators.required, Validators.maxLength(8)]),
+    email: new FormControl("",[Validators.required, Validators.email]),
+    password: new FormControl("",[Validators.required, Validators.minLength(8),Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')]),
+    passwordConfirm: new FormControl("",[Validators.required, Validators.minLength(8),Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')]),
     
   });
   constructor() { }
@@ -29,7 +29,34 @@ export class RegisterComponent implements OnInit {
 
   registrar(){
     console.log(this.formRegister.value);
+    console.log(this.formRegister.valid);
     
   }
 
+  get identidad(){
+    return this.formRegister.get('identidad');
+  }
+
+  get password(){
+    return this.formRegister.get('password');
+  }
+
+  get passwordConfirm(){
+    return this.formRegister.get('passwordConfirm')?.value;
+  }
+
+  get email(){
+    return this.formRegister.get('email');
+  }
+
+  get telefono(){
+    return this.formRegister.get('telefono');
+  }
+
+  get name(){
+    return this.formRegister.get('primerNombre');
+  }
+  get apellido(){
+    return this.formRegister.get('primerApellido');
+  }
 }
