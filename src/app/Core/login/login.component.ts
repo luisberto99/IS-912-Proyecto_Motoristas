d78ed448ed149dbb6d59e0df9b6aa34eb0a6c332
Luis Banegas <55450718+Shadow504@users.noreply.github.com>
2021-07-23 22:34:17 -0600

    Formulario, rutas y validaciones listas

--- src/app/Core/login/login.component.ts
+++ src/app/Core/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,50 @@ import { FormGroup, Validators, FormControl } from '@angular/forms';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
+  user:any = {
+    email: 'luisbanegas@gmail.com',
+    password: 'Dw2021##'
+  }
 
-  constructor() { }
+  formLogin = new FormGroup({
+    email: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required, Validators.minLength(8),Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')])
+  });
+
+
+  constructor(private route:Router) { }
 
   ngOnInit(): void {
   }
+
+  login(){
+    if(this.email?.value == this.user.email && this.password?.value == this.user.password && this.formLogin.valid == true){
+      console.log('usuario valido');
+      // this.route.navigate(['login']);
+    }else{
+      console.log(this.formLogin.valid);
+      
+      this.route.navigate(['login']);
+      // this.formLogin.value 
+
+      this.email?.setValue('');
+      this.password?.setValue('');
+
+      
+    }
+  }
+
+  recoverPassword(){
+    console.log('recuperar contraseña');
+  }
+
+  get email(){
+    return this.formLogin.get('email');
+  }
+
+  get password(){
+    return this.formLogin.get('password');
+  }
+
 
 }
