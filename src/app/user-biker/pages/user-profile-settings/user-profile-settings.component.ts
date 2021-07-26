08c121936f6c1928f4ade5c0d176e806bfa95b42
Luis Banegas <55450718+Shadow504@users.noreply.github.com>
2021-07-25 20:49:53 -0600

    Vista principal creada, con estilos y algunas funcionalidades

--- src/app/user-biker/pages/user-profile-settings/user-profile-settings.component.ts
+++ src/app/user-biker/pages/user-profile-settings/user-profile-settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile-settings',
@@ -6,13 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile-settings.component.sass']
 })
 export class UserProfileSettingsComponent implements OnInit {
-
-  constructor() { }
+  imgProfile:string = 'assets/imgs/avatar.png';
+  clickBtn:number = 0;
+  previsualizacion:any = '';
+  constructor(private sanitizer: DomSanitizer, private route:Router) { }
 
   ngOnInit(): void {
   }
 
   return(){
+    this.route.navigate(['/biker/ordersTaken']);
+  }
+
+  imgsUpload(event:any){
+
+    var data:File = event.target.files[0];
+    var binaryData = [];
+    binaryData.push(data);
+    const a = window.URL.createObjectURL(new Blob(binaryData, {type: "application/zip"}))
+    const image = this.sanitizer.bypassSecurityTrustUrl(a);
+    this.previsualizacion = image;
+  }
+
+
+  goPhotoProfile(){
+    this.clickBtn = 1;
+  }
+  
+  goProfile(){
+    this.clickBtn = 2;
     
+  }
+  
+  goSecurityProfile(){
+    this.clickBtn = 3;
+
   }
 }
