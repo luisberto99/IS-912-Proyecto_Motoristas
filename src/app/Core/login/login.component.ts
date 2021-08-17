d60c2f2eea7e01a3c6e432e8b95e214bd8530400
Luis Banegas <55450718+Shadow504@users.noreply.github.com>
2021-08-16 23:11:00 -0600

    Login terminado

--- src/app/Core/login/login.component.ts
+++ src/app/Core/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
+import { RegisterLoginService } from 'src/app/services/register-login/register-login.service';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +13,7 @@ export class LoginComponent implements OnInit {
     email: 'luisbanegas@gmail.com',
     password: 'Dw2021##'
   }
+  viewErrorLogin = false;
 
   formLogin = new FormGroup({
     email: new FormControl('',[Validators.required, Validators.email]),
@@ -19,26 +21,37 @@ export class LoginComponent implements OnInit {
   });
 
 
-  constructor(private route:Router) { }
+  constructor(private route:Router, private loginService:RegisterLoginService) { }
 
   ngOnInit(): void {
   }
 
   login(){
-    if(this.email?.value == this.user.email && this.password?.value == this.user.password && this.formLogin.valid == true){
-      console.log('usuario valido');
-      this.route.navigate(['biker']);
-    }else{
-      console.log(this.formLogin.valid);
+    // if(this.email?.value == this.user.email && this.password?.value == this.user.password && this.formLogin.valid == true){
+    //   console.log('usuario valido');
+    //   this.route.navigate(['biker']);
+    // }else{
+    //   console.log(this.formLogin.valid);
       
-      this.route.navigate(['login']);
-      // this.formLogin.value 
+    //   this.route.navigate(['login']);
+    //   // this.formLogin.value 
+    //   this.email?.setValue('');
+    //   this.password?.setValue('');
+    // }
 
-      this.email?.setValue('');
-      this.password?.setValue('');
+    this.loginService.verificarExisteCuenta(this.email?.value).subscribe(result =>{
+      // console.log(result);
+      if(result){
 
-      
-    }
+        this.loginBiker();
+
+      }else{
+        this.email?.setValue('');
+        this.password?.setValue('');
+        this.viewErrorLogin = true;
+      }
+    })
+
   }
 
   recoverPassword(){
@@ -53,5 +66,20 @@ export class LoginComponent implements OnInit {
     return this.formLogin.get('password');
   }
 
+  loginBiker(){
+    this.loginService.login({email:this.email?.value,password:this.password?.value}).subscribe(res =>{
+      // console.log(res, "login");
+      if(res.result){
+        let localStorage = window.localStorage;
+        localStorage.setItem('user',res.id);
+        this.formLogin.reset();
+        this.route.navigate(['/biker/ordersAvailable']);
+      }else{
+        this.viewErrorLogin = true;
+        this.formLogin.reset();
+      }
+      
+    })
+  }
 
 }
--- src/app/services/register-login/register-login.service.ts
+++ src/app/services/register-login/register-login.service.ts
@@ -9,9 +9,19 @@ export class RegisterLoginService {
   constructor(private http:HttpClient) { }
 
   urlRegistrar = "http://localhost:3000/motoristas/registrar";
+  urlLogin = "http://localhost:3000/motoristas/login"
 
   registrarNuevoMotorista(data:any){
     return this.http.post<any>(this.urlRegistrar,data)
+  }
+
+  verificarExisteCuenta(email:string){
+    let url = `http://localhost:3000/motoristas/obtenerDatos/${email}`;
+    return this.http.get<Boolean>(url);
+  }
+
+  login(data:any){
+    return this.http.post<any>(this.urlLogin,data);
   }
 
 }
